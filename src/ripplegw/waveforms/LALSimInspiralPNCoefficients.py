108c435be0504288ca531c811d1b3e274e3fbb00
Harsh Narola <hnarola@wn-lot-001.nikhef.nl>
2025-12-29 15:22:06 +0100

    Jit decorator for PN coefficients

--- src/ripplegw/waveforms/LALSimInspiralPNCoefficients.py
+++ src/ripplegw/waveforms/LALSimInspiralPNCoefficients.py
@@ -1,4 +1,5 @@
 import jax.numpy as jnp
+from jax import jit
 
 # Constants (from LAL)
 LAL_PI = jnp.pi
@@ -16,176 +17,198 @@ LAL_NUM_ST4_VARIABLES = 14
 XLALSimInspiralSpinDot_4PNS2CoeffAvg=0.5
 XLALSimInspiralSpinDot_4PNS2OCoeffAvg=-1.5
 
-
+@jit
 def XLALSimInspiralTaylorT4wdot_7PNCoeff(eta):
     return (LAL_PI/12096.0) * (-13245.0 + 717350.0*eta + 731960.0*eta*eta)
 
-
+@jit
 def XLALSimInspiralTaylorT4wdot_6PNCoeff(eta):
     return ( 16447.322263/139.7088 - 1712./105.
                 * LAL_GAMMA - 561.98689/2.17728 * eta + LAL_PI * LAL_PI
                 * (16./3. + 451./48. * eta) + 541./896. * eta * eta
                 - 5605./2592. * eta * eta * eta - 856./105. * jnp.log(16.) )
 
+@jit
 def XLALSimInspiralTaylorT4wdot_6PNLogCoeff(eta):
     return -(1712.0/105.0)
 
-
+@jit
 def XLALSimInspiralTaylorT4wdot_5PNCoeff(eta):
     return ( -(1.0/672.0) * LAL_PI * (4159.0 + 15876.0*eta) )  
 
+@jit
 def XLALSimInspiralTaylorT4wdot_4PNCoeff(eta):
     return (34103. + 122949.*eta + 59472.*eta*eta)/18144.
- 
+
+@jit
 def XLALSimInspiralTaylorT4wdot_3PNCoeff(eta):
     return 4.0 * LAL_PI
 
+@jit
 def XLALSimInspiralTaylorT4wdot_2PNCoeff(eta):
     return ( -(1.0/336.0) * (743.0 + 924.0*eta) )
 
+@jit
 def XLALSimInspiralTaylorT4wdot_0PNCoeff(eta):
     return 96. / 5. * eta
 
+@jit
 def XLALSimInspiralTaylorT4wdot_10PNTidalCoeff(mByM):
 
         return 6.*mByM*mByM*mByM*mByM * (12.-11.*mByM)
 
-
+@jit
 def XLALSimInspiralTaylorT4wdot_12PNTidalCoeff(mByM):
 
         return mByM*mByM*mByM*mByM * (4421./56. - 12263./56.*mByM + 1893./4.*mByM*mByM - 661./2.*mByM*mByM*mByM)
  
-
+@jit
 def XLALSimInspiralLDot_3PNSOCoeff(mByM):
     return 0.5+1.5/mByM
 
-
+@jit
 def XLALSimInspiralSpinDot_6PNS2CoeffAvg(mByM):
     return XLALSimInspiralSpinDot_6PNS2Coeff(mByM)+0.5*(XLALSimInspiralSpinDot_6PNS2nCoeff(mByM)+XLALSimInspiralSpinDot_6PNS2vCoeff(mByM))
 
+@jit
 def XLALSimInspiralSpinDot_6PNS2Coeff(mByM):
 
     return -1.5 -mByM
 
+@jit
 def XLALSimInspiralSpinDot_6PNS2nCoeff(mByM):
      return 1.5 +2.*mByM+mByM*mByM
 
-
+@jit
 def XLALSimInspiralSpinDot_6PNS2vCoeff(mByM):
      return 1.5 +mByM
 
-
+@jit
 def XLALSimInspiralSpinDot_6PNS2OCoeffAvg(mByM):
     return -0.5*(XLALSimInspiralSpinDot_6PNS2nCoeff(mByM)+XLALSimInspiralSpinDot_6PNS2vCoeff(mByM))
 
+@jit
 def XLALSimInspiralSpinDot_6PNS1OCoeffAvg(mByM):
     return -0.5*(XLALSimInspiralSpinDot_6PNS1nCoeff(mByM)+XLALSimInspiralSpinDot_6PNS1vCoeff(mByM))
 
-
+@jit
 def XLALSimInspiralSpinDot_6PNS1nCoeff(mByM):
      return 3.5-3./mByM-.5*mByM*mByM
 
+@jit
 def XLALSimInspiralSpinDot_6PNS1vCoeff(mByM):
      return 3. -1.5*mByM-1.5/mByM
 
+@jit
 def XLALSimInspiralSpinDot_6PNQMSOCoeffAvg(mByM):
     return -0.5*(XLALSimInspiralSpinDot_6PNQMSnCoeff(mByM)+XLALSimInspiralSpinDot_6PNQMSvCoeff(mByM));
 
-
+@jit
 def XLALSimInspiralSpinDot_6PNQMSnCoeff(mByM):
      return 3. * (.5/mByM + 1. - mByM - .5*mByM*mByM)
 
+@jit
 def XLALSimInspiralSpinDot_6PNQMSvCoeff(mByM):
      return 3. * (1./mByM -1.)
 
 
-
+@jit
 def XLALSimInspiralSpinDot_4PNQMSOCoeffAvg(mByM):
     return 1.5 * (1. - 1./mByM)
 
-
+@jit
 def XLALSimInspiralSpinDot_5PNCoeff(mByM):
     return 9./8. - mByM/2. + 7.*mByM*mByM/12. - 7.*mByM*mByM*mByM/6. - mByM*mByM*mByM*mByM/24.
 
+@jit
 def XLALSimInspiralL_3PNSicoeffAvg(mByM):
     return -0.75-0.25/mByM
 
-
+@jit
 def XLALSimInspiralL_3PNSiLcoeffAvg(mByM):
     return -(1./3.+9./mByM)/4.
 
-
+@jit
 def XLALSimInspiralSpinDot_3PNCoeff(mByM):
     return 3./2. -mByM - mByM*mByM/2.
 
 
-
+@jit
 def XLALSimInspiralPNEnergy_3PNSOCoeff(mByM):
      return 2. / 3. + 2. / mByM
 
-
+@jit
 def XLALSimInspiralPNEnergy_4PNS1S2CoeffAvg(eta):
      return 1./eta
 
-
+@jit
 def XLALSimInspiralPNEnergy_4PNS1OS2OCoeffAvg(eta):
      return -3./eta
 
-
+@jit
 def XLALSimInspiralPNEnergy_4PNQMS1S1CoeffAvg(mByM):
      return .5/mByM/mByM
 
+@jit
 def XLALSimInspiralPNEnergy_4PNQMS1OS1OCoeffAvg(mByM):
      return -1.5/mByM/mByM
 
-
+@jit
 def XLALSimInspiralPNEnergy_5PNSOCoeff(mByM):
      return 5./3. + 3./mByM + 29.*mByM/9. + mByM*mByM/9.
 
-
+@jit
 def XLALSimInspiralPNEnergy_6PNS1S2CoeffAvg(eta):
     return 2./eta -11./6.
 
+@jit
 def XLALSimInspiralPNEnergy_6PNS1OS2OCoeffAvg(eta):
     return -11./3./eta + 2.3/1.8
 
+@jit
 def XLALSimInspiralPNEnergy_6PNS1S1CoeffAvg(mByM):
      return -1./(mByM*mByM) - 1./6./mByM -0.5
 
+@jit
 def XLALSimInspiralPNEnergy_6PNQMS1S1CoeffAvg(mByM):
      return 1.25/mByM/mByM + 1.25/mByM + 5./12.
 
 
-
+@jit
 def XLALSimInspiralPNEnergy_6PNS1OS1OCoeffAvg(mByM):
      return 6./(mByM*mByM) -1.5/mByM -1.1/1.8
 
+@jit
 def XLALSimInspiralPNEnergy_6PNQMS1OS1OCoeffAvg(mByM):
      return -3.75/mByM/mByM - 3.75/mByM - 1.25
 
+@jit
 def XLALSimInspiralPNEnergy_7PNSOCoeff(mByM):
      return -75./4. + 27./(4.*mByM) + 53.*mByM/2. + 67*mByM*mByM/6. + 17.*mByM*mByM*mByM/12. - mByM*mByM*mByM*mByM/12.
 
 
-
+@jit
 def XLALSimInspiralPNEnergy_10PNTidalCoeff(mByM):
      return -9.0 * mByM*mByM*mByM*mByM*(1.-mByM)
 
+@jit
 def XLALSimInspiralPNEnergy_12PNTidalCoeff(mByM):
      return 6./(mByM*mByM) -1.5/mByM -1.1/1.8
 
 
-
+@jit
 def XLALSimInspiralPNEnergy_2PNCoeff(eta):
      return -(0.75 + eta/12.0)
+
+@jit
 def XLALSimInspiralPNEnergy_4PNCoeff(eta):
      return -(27.0/8.0 - 19.0/8.0 * eta + 1./24.0 * eta*eta)
 
-
+@jit
 def XLALSimInspiralPNEnergy_6PNCoeff(eta):
      return -(67.5/6.4 - (344.45/5.76 - 20.5/9.6 * LAL_PI*LAL_PI) * eta + 15.5/9.6 * eta*eta + 3.5/518.4 * eta*eta*eta)
 
-
+@jit
 def compute_wdotspin(params, LNhdotS1, LNhdotS2, S1dotS2, S1sq, S2sq, spinO):
     """
     Compute spin corrections to domega based on spin order.
@@ -269,6 +292,7 @@ def compute_wdotspin(params, LNhdotS1, LNhdotS2, S1dotS2, S1sq, S2sq, spinO):
 
 
 # Then in your setup function, add these coefficients:
+@jit
 def add_wdot_spin_coefficients_to_setup(m1M, m2M, eta, quadparam1, quadparam2):
     """
     Add wdot spin coefficients to params dictionary.
@@ -329,75 +353,69 @@ def add_wdot_spin_coefficients_to_setup(m1M, m2M, eta, quadparam1, quadparam2):
     
     return params_dict
 
-
+@jit
 def XLALSimInspiralTaylorT4wdot_6PNS1OS2OCoeffAvg(eta):
      return 162.25/(2.24*eta) - 129.31/2.88
 
+@jit
 def XLALSimInspiralTaylorT4wdot_4PNS1OS2OCoeffAvg(eta):
      return 721. / 48. / eta
 
+@jit
 def XLALSimInspiralTaylorT4wdot_3PNSOCoeff(mByM):
     return - 19./6. - 25./4./mByM
 
-
+@jit
 def XLALSimInspiralTaylorT4wdot_4PNS1S2CoeffAvg(eta):
     return - 247. / 48. / eta
 
-
+@jit
 def XLALSimInspiralTaylorT4wdot_4PNS1S1CoeffAvg(mByM):
     return  7./96./mByM/mByM
 
+
+@jit
 def XLALSimInspiralTaylorT4wdot_4PNQMS1S1CoeffAvg(mByM):
     return -2.5/mByM/mByM
 
+@jit
 def XLALSimInspiralTaylorT4wdot_4PNS1OS1OCoeffAvg(mByM):
     return -1./96./mByM/mByM
 
+@jit
 def XLALSimInspiralTaylorT4wdot_4PNQMS1OS1OCoeffAvg(mByM):
     return 7.5/mByM/mByM
 
+@jit
 def XLALSimInspiralTaylorT4wdot_5PNSOCoeff(mByM):
     return -809./(84.*mByM) + 13.795/1.008 - 527.*mByM/24. - 79.*mByM*mByM/6.
 
+@jit
 def XLALSimInspiralTaylorT4wdot_6PNSOCoeff(mByM):
     return  jnp.pi * ( -37./3. - 151./6./mByM )
 
+@jit
 def XLALSimInspiralTaylorT4wdot_6PNS1S2CoeffAvg(eta):
     return 108.79/(6.72*eta) + 75.25/2.88
 
+@jit
 def XLALSimInspiralTaylorT4wdot_6PNS1S1CoeffAvg(mByM):
     return 101.9/(6.4*mByM*mByM) + 2.51/(5.76*mByM) + 13.33/5.76
 
+@jit
 def XLALSimInspiralTaylorT4wdot_6PNQMS1S1CoeffAvg(mByM):
     return -6.59/(2.24*mByM*mByM) + 7.3/(4.8*mByM) - 43./4.
 
+@jit
 def XLALSimInspiralTaylorT4wdot_6PNS1OS1OCoeffAvg(mByM):
     return -49.3/(6.4*mByM*mByM) + 197.47/(5.76*mByM) + 56.45/5.76
 
+@jit
 def XLALSimInspiralTaylorT4wdot_6PNQMS1OS1OCoeffAvg(mByM):
     return 19.77/(2.24*mByM*mByM) - 7.3/(1.6*mByM) + 129./4.
 
-
+@jit
 def XLALSimInspiralL_2PN(eta):
 
     return 1.5 + eta/6.
 
-'''
-wspin3 = params->wdot3S1O*LNhdotS1 + params->wdot3S2O*LNhdotS2;
-
-wspin4Avg = params->wdot4S1S2Avg *S1dotS2 + params->wdot4S1OS2OAvg *LNhdotS1 * LNhdotS2;
-wspin4Avg += (params->wdot4S1S1Avg + params->wdot4QMS1S1Avg) * S1sq
-	      + (params->wdot4S2S2Avg + params->wdot4QMS2S2Avg) * S2sq
-	      + (params->wdot4S1OS1OAvg + params->wdot4QMS1OS1OAvg) * LNhdotS1 * LNhdotS1
-	      + (params->wdot4S2OS2OAvg + params->wdot4QMS2OS2OAvg) * LNhdotS2 * LNhdotS2;
-
-wspin5 = params->wdot5S1O*LNhdotS1 + params->wdot5S2O*LNhdotS2;
-
-wspin6Avg = params->wdot6S1O*LNhdotS1 + params->wdot6S2O*LNhdotS2
-	  + params->wdot6S1S2Avg*S1dotS2 + params->wdot6S1OS2OAvg*LNhdotS1*LNhdotS2
-	  + (params->wdot6S1S1Avg + params->wdot6QMS1S1Avg)*S1sq
-	  + (params->wdot6S2S2Avg + params->wdot6QMS2S2Avg)*S2sq
-	  + (params->wdot6S1OS1OAvg + params->wdot6QMS1OS1OAvg)*LNhdotS1*LNhdotS1 +
-	  + (params->wdot6S2OS2OAvg + params->wdot6QMS2OS2OAvg)*LNhdotS2*LNhdotS2;
-
-'''
